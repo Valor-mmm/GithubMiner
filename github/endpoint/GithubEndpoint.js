b56c88cb634478a797e3c302b0691ece266051ea
valor-mmm <stefan1.hauser@st.oth-regensburg.de>
2018-10-29 11:16:12 +0100

    Creating a simple GithubApi Endpoint for queries

---
+++ github/endpoint/GithubEndpoint.js
@@ -0,0 +1,59 @@
+const {GraphQLClient} = require('graphql-request');
+
+class GithubEndpoint {
+
+    static async callEndpoint(apiConfig, options, query) {
+        const client = GithubEndpoint.createGraphQLClient(apiConfig, options);
+        const data = await client.request(query);
+        console.log(JSON.stringify(data, null, 2));
+        return data;
+    }
+
+    static createGraphQLClient(apiConfig, options) {
+        if (!apiConfig || !apiConfig.endpointUrl) {
+            console.error('API config with url needed but not specified correctly.');
+        }
+
+        const mergedOptions = GithubEndpoint.mixOptions(apiConfig, options);
+        console.log(mergedOptions);
+
+        return new GraphQLClient(apiConfig.endpointUrl, mergedOptions);
+    }
+
+    static mixOptions(apiConfig, options) {
+        if (!apiConfig) {
+            console.warn('No api config or apiToken given. Nothing there to mix in.');
+            if (!options) {
+                return {};
+            }
+            return options;
+        }
+
+        let authorizationHeader = {};
+        if (apiConfig.apiToken) {
+            authorizationHeader = { Authorization: `Bearer ${apiConfig.apiToken}` };
+        }
+
+        if (options && options.headers && options.headers.authorization) {
+            console.warn('Authorization header would have been overwritten. Skipping merge!');
+            return options;
+        }
+
+        if (!options) {
+            return {headers: authorizationHeader};
+        }
+
+        if (options.headers) {
+            options.headers = {...options.headers, ...authorizationHeader};
+            return options;
+        }
+
+        if (Object.keys(authorizationHeader).length === 0 && authorizationHeader.constructor === Object) {
+            return options;
+        }
+
+        return {...options, ...{headers: authorizationHeader}};
+    }
+}
+
+module.exports = GithubEndpoint;
--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 const GithubConfigProvider = require('./github/endpoint/config/GithubConfigProvider');
+const GithubEndpoint = require('./github/endpoint/GithubEndpoint');
+
 
 const config = GithubConfigProvider.readConfig();
-console.log(config);
+
+const query = `query { 
+  repository(owner:"Valor-mmm", name:"Ranner-Backend") {
+    name
+  }
+}`;
+
+GithubEndpoint.callEndpoint(config, null, query).then(res => console.log(res)).catch(err => console.error(err));
