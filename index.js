98a99b6825ae3f65f72a7fb0e8c116528230215a
valor-mmm <stefan1.hauser@st.oth-regensburg.de>
2018-11-05 13:54:35 +0100

    Adding dataset with 1500 repositories encoded in json

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ const RepoListQueryExecutor = require('./execute/RepoListQueryExecutor');
 const PopularityIndexQueryType = require('./github/query/types/PopularityIndexQueryType');
 
 
-const endpointPromise = RepoListQueryExecutor.execute(new PopularityIndexQueryType(), null, null);
+const endpointPromise = RepoListQueryExecutor.execute(new PopularityIndexQueryType(), './repoList/1500Refurbished.json', {propertyName: 'repo_name'});
 endpointPromise.then(res => {console.log(res)}).catch(err => console.error('Error: ' + err));
