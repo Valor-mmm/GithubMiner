0dc574f0a729c20400ee0dee1af8e5518c872b6d
valor-mmm <stefan1.hauser@st.oth-regensburg.de>
2018-11-07 07:57:30 +0100

    Cleanup task

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const RepoListQueryExecutor = require('./execute/RepoListQueryExecutor');
 const PopularityIndexQueryType = require('./github/query/types/PopularityIndexQueryType');
 
 
-const endpointPromise = RepoListQueryExecutor.execute(new PopularityIndexQueryType(), './repoList/1500Refurbished.json', {propertyName: 'repo_name', outputDirPath: './abcd'});
+const config = {propertyName: 'repo_name', outputDirPath: './separation200/', separationSize: 200};
+const endpointPromise = RepoListQueryExecutor.execute(new PopularityIndexQueryType(), './repoList/14000Names.json', config);
 endpointPromise.then(console.log('Started execution.'))
     .catch(err => console.error('Error during execution start. ' + err));
