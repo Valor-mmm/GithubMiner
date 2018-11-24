8eaa5eef2832f5722978a5039db3d1afc20b33f6
valor-mmm <stefan1.hauser@st.oth-regensburg.de>
2018-11-24 16:08:46 +0100

    Resetting index to default usage

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const RepoListQueryCaller = require('./execute/caller/RepoListQueryCaller').Repo
 const PaginationContributorQueryType = require('./github/query/types/paginationQueries/PaginationContributorQueryType').PaginationContributorQueryType;
 const logger = require('./LoggerProvider').getLogger('Index');
 
-const config = {propertyName: 'repo_name', outputDirPath: './resultObjectify/', separationSize: 20};
+const config = {propertyName: 'repoName', outputDirPath: './result/', separationSize: 20};
 
-const endpointPromise = RepoListQueryCaller.call(new PaginationContributorQueryType(), './repoList/results-20181112-154553_objectify.json', config);
+const endpointPromise = RepoListQueryCaller.call(new PaginationContributorQueryType(), './repoList/repoList.json', config);
 endpointPromise.then(() => logger.info('Finished initialization.'))
     .catch(err => logger.error('Error during execution: ', err));
